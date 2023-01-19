33611ff8f5a06d1d81805f65768db674c1308084
rahul kumar <rkrahul8181@gmail.com>
2023-01-19 17:11:54 +0530

    backend

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const DB_URL= `mongodb+srv://r:f@cluster0.cyy5u.mongodb.net/fw190480-mock11?retryWrites=true&w=majority`
 const config = async()=>{
-    await mongoose.connect(DB_URL)
+    amongoose.set("strictQuery", false);
+    mongoose.connect(DB_URL,{useNewUrlParser: true})
 }
 
 module.exports = config
